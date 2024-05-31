8195c6eef1f36f9c7dce42c6e5360fae7205a1a4
SAJEEA POOZHITHARA <psajeea@gmail.com>
2024-05-31 15:33:41 +0800

    file upload

--- src/model/productSchema.js
+++ src/model/productSchema.js
@@ -1,76 +1,58 @@
-const mongoose = require('mongoose');
-const { path } = require('../../app');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
-
-const productSchema = new Schema({
-
-    product_name: {
-        type: String,
-        required : true,
-        unique: true,
+const productSchema = new mongoose.Schema({
+ 
+    name:{
+        type:String,
+        require:true,
     },
-    price: {
-      type: Number,
+    price:{
+        type:Number
     },
-
-    actualPrice: {
-      type: Number,
-      required: true,
+    description:{
+        type:String,
+        required:true,
     },
-    sellingPrice: {
-      type: Number,
-      required: true,
-    },
-    
     category: {
         type: mongoose.Schema.Types.ObjectId,
-        required : true,
-       
-    },
-
-    stock:{
-      type:Number,
-      min:0,
-      requires:true,
-      default:0
-  },
-
-
-    description: {
-      type: String,
-      required: true,
-    },
-    
-    primary_image: {
-        name: {
-          type: String,
-        },
-        path: {
-          type: String,
-        },
-      },
-      secondary_images: [
-        {
-          name: {
-            type: String,
-          },
-          path: {
-            type: String,
-          },
-        },
-      ],
-      
-      
-    isActive: {
-        type: Boolean,
+        ref: "Category",
         required: true,
-        default: true,
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    stock:{
+        type:Number,
+        min:0,
+        requires:true,
+        default:0
     },
 
+    primaryImages:[
+        {
+            name:{
+                type:String,
+            },
+            path:{
+                type:String
+            }
+        }
+    ],
+    secondaryImages:[
+        {
+            name:{
+                type:String,
+            },
+            path:{
+                type:String
+            }
+        }
+    ],
 },
 {
-    timestamps: true
-});
-
-module.exports = mongoose.model('Product', productSchema)
+    timestamps: true,
+    strict: false,
+  },
+)
+module.exports = mongoose.model("Product", productSchema);
