af61c4e4d2fc21b29619ea1621a4537568531c50
sajeeap <152425015+sajeeap@users.noreply.github.com>
2024-09-03 00:28:56 +0800

    Update passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://localhost:${process.env.APP_PORT}/auth/google/callback`
+    callbackURL: `http://13.236.94.222:${process.env.APP_PORT}/auth/google/callback`
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
